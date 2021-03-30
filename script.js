44c8c91aa9e5904a61cd6706785395548d9ac735
JustSemen <81112954+JustSemen@users.noreply.github.com>
2021-03-30 14:57:16 +0300

    Загрузка первых файлов программы

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿let pixels = document.querySelectorAll('.pixel');
+let color = document.querySelector('.chosen-color');
+let eraser = document.querySelector('.eraser');
+
+
+for(let pixel of pixels) {
+  pixel.onclick = function () {
+    if (eraser.checked) {
+      pixel.style.backgroundColor = 'white'; 
+    } else {
+      pixel.style.backgroundColor = color.value;
+    }
+  }
+}
